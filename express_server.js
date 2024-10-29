6ccff52b62936350464e53fa1af219d590bdc263
MPNose <mpnoseworthy89@gmail.com>
2024-10-28 21:05:53 -0700

    created urls_new, linked app.post through server, tested

--- express_server.js
+++ express_server.js
@@ -4,20 +4,32 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars)
-})
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -34,3 +46,8 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+  let random = Math.random().toString(36);
+   return random.slice(2, 8);
+};
