e1a62fad45218196e3469dec200fd447b3917f52
MPNose <mpnoseworthy89@gmail.com>
2024-10-31 13:14:38 -0700

    added more permission features

--- express_server.js
+++ express_server.js
@@ -15,8 +15,14 @@ const generateRandomString = function() {
 };
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  sgq3y6: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -30,7 +36,25 @@ const users = {
     email: "b@b.com",
     password: "54321",
   },
+  aj48lW: {
+    id: 'aj48lW',
+    email: 'd@d.com',
+    password: "12345"
+  }
 };
+
+const urlsForUser = function(id) {
+  const userURLs = {};
+  for (const key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      userURLs[key] = {
+        longURL: urlDatabase[key].longURL,
+        userID: urlDatabase[key].userID
+      }
+    }
+  }
+  return userURLs;
+}
 
 const getUserByEmail = function(email) {
   for (const userId in users) {
@@ -56,14 +80,22 @@ app.post("/urls", (req, res) => {
     return res.send("You need an account to use TinyApp");
   }
   let shortId = generateRandomString();
-  urlDatabase[shortId] = req.body.longURL;
-  res.redirect(`/urls/${shortId}`); // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortId] = {
+    longURL: req.body.longURL,
+    userID: req.cookies['userId']
+  };
+  res.redirect(`/urls/${shortId}`); 
 });
 
 app.get("/urls", (req, res) => {
+  if (!req.cookies['userId']) {
+    return res.send("You need to login or register in order to see URLs");
+  }
+  const urls = urlsForUser(req.cookies["userId"]);
+  
   const templateVars = { 
     user: users[req.cookies.userId],
-    urls: urlDatabase };
+    urls, };
   res.render("urls_index", templateVars);
 });
 
@@ -78,10 +110,16 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
+  if (!req.cookies['userId']) {
+    return res.send('You need to be logged in to see URLs');
+  }
+  if (req.cookies['userId'] !== urlDatabase[req.params.id].userID) {
+    return res.status(401).send('This URL does not belong to you');
+  }
   const templateVars = {
     user: users[req.cookies.userId],
     id: req.params.id, 
-    longURL: urlDatabase[req.params.id]};
+    longURL: urlDatabase[req.params.id].longURL};
   res.render("urls_show", templateVars)
 });
 
@@ -89,26 +127,32 @@ app.get("/u/:id", (req, res) => {
   if (!Object.keys(urlDatabase).includes(req.params.id)) {
     return res.send("The URL you have requested is not in the database");
   }
-  const longURL = urlDatabase[req.params.id];
+  const longURL = urlDatabase[req.params.id].longURL;
   res.redirect(longURL);
 });
 
-app.post("/urls/:id/delete", (req, res) => {
-  const templateVars = {
-    user: users[req.cookies.userId]
-  };
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
-});
 
 app.post("/urls/:id", (req, res) => {
-  const templateVars = {
-    user: users[req.cookies.userId]
-  };
+  if (req.cookies['userId'] !== urlDatabase[req.params.id].userID || !req.cookies['userId']) {
+    return res.status(401).send("You cannot delete this URL");
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.send("URL does not exist");
+  }
   const id = req.params.id;
   const newLongURL = req.body.newLongURL;
   urlDatabase[id] = newLongURL;
   res.redirect('/urls');
+});
+app.post("/urls/:id/delete", (req, res) => {
+  if (req.cookies['userId'] !== urlDatabase[req.params.id].userID || !req.cookies['userId']) {
+    return res.status(401).send("You cannot delete this URL");
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.send("URL does not exist");
+  }
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
 });
 
 app.post("/login", (req, res) => {
